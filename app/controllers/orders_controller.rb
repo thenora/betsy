c01645e3cbc86cfc0595b5affbfb5453afc3019c
Jessica Liang <jessicursliang@gmail.com>
2020-06-09 15:30:36 -0700

    generate order items controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,20 @@
 class OrdersController < ApplicationController
+
+	def index
+		@order = orders.all
+	end
+		
+
+	def show
+		if @order.nil?
+			head :not_found
+			return
+		end
+	end
+
+    def create
+        @order = Order.new()
+    
+    end
 end
+
