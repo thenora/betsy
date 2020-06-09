ea7c0a6dc34a4f233092abe32aca352be3fd5cf9
Jessica Liang <jessicursliang@gmail.com>
2020-06-09 15:51:32 -0700

    set up create for OrderItem

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,22 +1,19 @@
 class OrderItemsController < ApplicationController
 
-	def index
-		@order_items = order_items.all
-	end
+	# def index
+	# 	@order_items = order_items.all
+	# end
 		
+	# def show
+	# 	if @order_item.nil?
+	# 		head :not_found
+	# 		return
+	# 	end
+	# end
 
-	def show
-		if @order_item.nil?
-			head :not_found
-			return
-		end
-	end
-
-    def create
-        @order = Order.new()
-    
-    end
-
+	def create
+			@order = Order.new()
 	
+	end
 
 end
