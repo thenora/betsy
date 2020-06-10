105ed3ffe61fc564a70d6304228915107fbeead2
thenora <thenora@gmail.com>
2020-06-09 22:06:45 -0700

    tweaked routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :products do
-    resources :order_items, only [:create, :destroy]
-  end
-
+  resources :products # do
+  #   resources :order_items, only [:create, :destroy]
+  # end
 
 end
