1beebcb5d8463d6ea52b6473ab704d8211f39806
kate myer <kate.d.mangubat@gmail.com>
2020-06-11 14:17:57 -0700

    need to finish order method (just notes)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,27 @@
 class OrdersController < ApplicationController
 
+	#TODO: 
+
+	#all orders
 	def index
 		@order = orders.all
 	end
 		
 
+	#individual order
 	def show
 		if @order.nil?
 			head :not_found
 			return
 		end
 	end
+
+	#mark order sent or closed
+	def delete
+		
+	end
+
+
 
 	def cart # show function for the open order at the moment
 		# session[:order][:id]
