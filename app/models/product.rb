1f01f7f5bd5d8d07b106e0224d5a676c67d25435
kate myer <kate.d.mangubat@gmail.com>
2020-06-11 14:18:23 -0700

    notes for kate for product relation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :merchant
   has_one :order_item
+  #need to clarify this because if there's 10 plants and all get purchased, should see 10 order items
+  #product has many order items.... 
+  #has_many: order_item
+
+  #PER Jess: quantity is an attribute of the model 
 end
+
+
+#product.order_items..will give all order items with this product  vs product.order_item ..this is just one
