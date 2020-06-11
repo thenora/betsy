21dc40acbf4722a30afddc26d1edb50e09af6d1c
thenora <thenora@gmail.com>
2020-06-10 23:29:35 -0700

    new product method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,14 @@
 class ProductsController < ApplicationController
+  # TODO setup login required
+  # skip_before_action :require_login, only: [:index, :show]
 
   def index
     @products = Product.all
+    # TODO add status active
   end
 
   def new
-
+    @product = Product.new
   end
 
   def create
