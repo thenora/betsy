c14f611c8cc2f2f25784c44ba269bb9f7de73b7f
thenora <thenora@gmail.com>
2020-06-10 23:59:53 -0700

    added create method to product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,19 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params) # Instantiate a new work
+    # TODO add merchant
+    # @product.merchant_id = session[:merchant_id]
 
+    if @product.save
+      flash[:success] = "Your product was added."
+      redirect_to product_path(@product.id)
+      return
+    else # if save fails
+      flash.now[:error] = "Oops. We couldn't add your product."
+      render :new, status: :bad_request # show the new media form again
+      return
+    end
   end
 
   def show
@@ -25,5 +37,11 @@ class ProductsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def product_params
+    return params.require(:product).permit(:name, :price, :description, :inventory, :status, :photo_url)
   end
 end
