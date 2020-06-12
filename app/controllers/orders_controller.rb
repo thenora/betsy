d8f80e578965726edc035e549fd2d21a0583a1b9
kate myer <kate.d.mangubat@gmail.com>
2020-06-11 22:25:31 -0700

    added Order index and show methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,20 @@
 class OrdersController < ApplicationController
 
+	#GET /orders
 	def index
-		@order = Order.all
+		@orders = Order.all
+		#  order = Order.all.as_json(only: [:id, :card_number,:card_expiration_date, :card_cvv, :address, :city,:zip_code, :guest_name, :email, :phone_num, :cart_status])
+		# render json: order, status: :ok
 	end
 		
+	#GET /orders/:id
 	def show
+		order_id = params[:id].to_i
+    @order = Order.find_by_id(order_id)
+  
 		if @order.nil?
 			head :not_found
-			return
+		return
 		end
 	end
 
