1a3d90b8eed861c4a7f94574e1a53c2ef7580264
thenora <thenora@gmail.com>
2020-06-13 03:09:03 -0700

    empty categories working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_13_095808) do
+ActiveRecord::Schema.define(version: 2020_06_13_100344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,13 +26,6 @@ ActiveRecord::Schema.define(version: 2020_06_13_095808) do
     t.bigint "category_id"
     t.index ["category_id"], name: "index_categories_products_on_category_id"
     t.index ["product_id"], name: "index_categories_products_on_product_id"
-  end
-
-  create_table "categories_products_joins", force: :cascade do |t|
-    t.bigint "book_id"
-    t.bigint "genre_id"
-    t.index ["book_id"], name: "index_categories_products_joins_on_book_id"
-    t.index ["genre_id"], name: "index_categories_products_joins_on_genre_id"
   end
 
   create_table "merchants", force: :cascade do |t|
