26194410875064ba6e133aa6371320314642cea3
kate myer <kate.d.mangubat@gmail.com>
2020-06-16 15:11:28 -0700

    Model test validations: Order done + passing

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,48 +2,50 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :products, through: :order_items
 
-  # validates :card_number,
-  #   presence: true,
-  #   length: { is: 16 }, 
-  #   format: { with: /[0-9]{16}/ },
-  #   :on => :update
-
-  # validates :card_expiration_date,
-  #   presence: true,
-  #   :on => :update
-
-  # validates :card_cvv,
-  #   presence: true,
-  #   length: { is: 3 },
-  #   format: { with: /[0-9]{3}/ },
-  #   :on => :update
-
-  # validates :address,
-  #   presence: true,
-  #   :on => :update
   
-  # validates :city,
-  #   presence: true,
-  #   :on => :update
+  validates :card_number,
+    presence: true,
+    length: { is: 16 }, 
+    format: { with: /[0-9]{16}/ },
+    :on => :update
 
-  # validates :zip_code,
-  #   presence: true,
-  #   format: { with: /[\d]{5}\-[\d]*/ },
-  #   :on => :update
+  validates :card_expiration_date,
+    presence: true,
+    :on => :update
 
-  # validates :guest_name,
-  #   presence: true,
-  #   :on => :update
+  validates :card_cvv,
+    presence: true,
+    length: { is: 3 },
+    format: { with: /[0-9]{3}/ },
+    :on => :update
 
-  # validates :email,
-  #   presence: true,
-  #   format: { with: /.+@.+\..+/ },
-  #   :on => :update
+  validates :address,
+    presence: true,
+    :on => :update
+  
+  validates :city,
+    presence: true,
+    :on => :update
 
-  # validates :phone_num,
-  #   presence: true,
-  #   format: { with: /\A.*(\d{3}).*(\d{3}).*(\d{4})\z/ },
-  #   :on => :update
+  validates :zip_code,
+    presence: true,
+    format: { with: /[\d]{5}/ },
+    :on => :update
+
+  
+  validates :guest_name,
+    presence: true,
+    :on => :update
+
+  validates :email,
+    presence: true,
+    format: { with: URI::MailTo::EMAIL_REGEXP },
+    :on => :update
+
+  validates :phone_num,
+    presence: true,
+    format: { with: /\([0-9]{3}\)[0-9]{3}-[0-9]{4}/ },
+    :on => :update
 
 
   def total_price
