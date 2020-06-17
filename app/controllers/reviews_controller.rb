3cf07b57c42f2622df27676bb94265d9f51d55d5
Jessica Liang <jessicursliang@gmail.com>
2020-06-17 15:41:10 -0700

    styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,30 @@
 class ReviewsController < ApplicationController
 
-	def index
-		product = Product.find_by(id: params[:product_id])
+	skip_before_action :verify_authenticity_token
+	skip_before_action :require_login
+
+  def create
+		@review = Review.new(
+			title: review_params[:title],
+			rating: review_params[:rating],
+			description: review_params[:description],
+			product_id: params[:product_id]
+		)
 		
+		if @review.save!
+			flash[:success] = "Your review was added."
+			redirect_back fallback_location: root_path
+			return
+		else
+			flash.now[:failure] = "Error: Review could not be added."
+			redirect_back fallback_location: root_path
+			return
+		end
+	end
+	
+	private
+
+	def review_params
+		return params.require(:review).permit(:title, :rating, :description, :product_id)
 	end
 end
