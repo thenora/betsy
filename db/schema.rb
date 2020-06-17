6a5eb45b66857fdf5ae67121ad2e951961e73c21
thenora <thenora@gmail.com>
2020-06-16 18:50:13 -0700

    deleted extra table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_214220) do
+ActiveRecord::Schema.define(version: 2020_06_17_014820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,13 +77,6 @@ ActiveRecord::Schema.define(version: 2020_06_15_214220) do
     t.boolean "status"
     t.string "photo_url"
     t.index ["merchant_id"], name: "index_products_on_merchant_id"
-  end
-
-  create_table "products_categories_joins", force: :cascade do |t|
-    t.bigint "product_id"
-    t.bigint "category_id"
-    t.index ["category_id"], name: "index_products_categories_joins_on_category_id"
-    t.index ["product_id"], name: "index_products_categories_joins_on_product_id"
   end
 
   add_foreign_key "order_items", "orders"
