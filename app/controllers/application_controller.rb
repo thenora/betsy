c4362c08c6ca1ec82c2253d5952d9116036b3326
thenora <thenora@gmail.com>
2020-06-17 01:04:45 -0700

    require_login method & skip_before on controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+  
+  def current_user
+    return User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+  
+  def require_login
+    if current_user.nil?
+      flash[:error] = "Oops. Looks like you don't have permission to view this page. If you're a merchant, please login."
+      redirect_to root_path
+    end
+  end
+
+
 end
