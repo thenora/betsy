7b90bb51b4e4e65f50af6f9f10bbd12cd0bf46a6
thenora <thenora@gmail.com>
2020-06-17 22:48:43 -0700

    undid merge

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -5,13 +5,13 @@ class MerchantsController < ApplicationController
     @merchants = Merchant.all
   end
 
-  # def show
-  #   @merchant = Merchant.find_by(id: params[:id])
-  #   if @merchant.nil?
-  #     head :not_found
-  #     return
-  #   end
-  # end
+  def show
+    @merchant = Merchant.find_by(id: params[:id])
+    if @merchant.nil?
+      head :not_found
+      return
+    end
+  end
 
   def create
     auth_hash = request.env["omniauth.auth"]
