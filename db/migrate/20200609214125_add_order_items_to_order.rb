d6e13a24eca0611badb708ec19dc008e36c0f42a
Jessica Liang <jessicursliang@gmail.com>
2020-06-18 16:57:15 -0700

    migration fix

--- db/migrate/20200609214125_add_order_items_to_order.rb
+++ db/migrate/20200609214125_add_order_items_to_order.rb
@@ -1,4 +1,4 @@
-class AddOrderItemstoOrder < ActiveRecord::Migration[6.0]
+class AddOrderItemsToOrder < ActiveRecord::Migration[6.0]
   def change
     add_reference :order_items, :order, foreign_key: true
   end
