d5c8b457bbed45c89d67f7bc5b8c9a25f2a52e91
thenora <thenora@gmail.com>
2020-06-18 17:54:45 -0700

    removed comment

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,6 @@ class Product < ApplicationRecord
   has_and_belongs_to_many :categories
   has_many :reviews
 
-  # ? Nora's thoughts: 
-  # ? has_many :order_items
-  # ? has_many :orders, through :order_items
-
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: {only_float: true, greater_than: 0}
   validates :inventory, presence: true, numericality: {only_integer: true} 
